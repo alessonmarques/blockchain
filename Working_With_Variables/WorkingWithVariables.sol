de7a4beaa6fe9d0984dbcbcaf003a2b945666be3
Alesson Marques <alessonmarques@live.com>
2021-12-07 02:23:37 -0300

    Working with boolean variables

--- Working_With_Variables/WorkingWithVariables.sol
+++ Working_With_Variables/WorkingWithVariables.sol
@@ -2,13 +2,20 @@ pragma solidity ^0.5.13;
 
 contract WorkingWithVariables {
     uint256 public myUint;
-    string public myNickname;
 
     function setMyUint (uint _myUint) public {
         myUint = _myUint;
     }
 
+    string public myNickname;
+
     function setMyNickname (string memory _myNickname) public {
         myNickname = _myNickname;
+    }
+
+    bool public myBool;
+
+    function setMyBool (bool _myBool) public {
+        myBool = _myBool;
     }
 }
